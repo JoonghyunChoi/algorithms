55d34fc0a3e4c1069d70df31c5ea54889bb6726c
JoonghyunChoi <wnd3735@gmail.com>
2022-07-26 00:02:25 +0900

    Update algorithms

--- algorithms2/graph/graph2/topological_sort.py
+++ algorithms2/graph/graph2/topological_sort.py
@@ -1,17 +1,30 @@
-def topological_sort(a):
+def topological_sort1(s, a):
     visited = [False] * len(a)
     b = []
 
     def dfs(u):
         visited[u] = True
-
         for v in a[u]:
             if not visited[v]:
                 dfs(v)
         b.append(u)
-
-    for i in range(len(a)):
-        if not visited[i]:
-            dfs(i)
+    dfs(s)
     b.reverse()
+    return b
+
+
+def topological_sort2(a, indegree):
+    queue = []
+    b = []
+    for u in range(len(a)+1):
+        if indegree[u] == 0:
+            queue.append(u)
+
+    while queue:
+        u = queue.pop(0)
+        b.append(u)
+        for v in a[u]:
+            indegree[v] -= 1
+            if indegree[v] == 0:
+                queue.append(v)
     return b
